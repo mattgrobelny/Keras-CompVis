e259407677da63a1de6fc4b70819ceab7522535b
mattgrobelny <mattgrobelny@gmail.com>
2017-04-21 15:11:52 -0500

    skip images working

--- click_label.py
+++ click_label.py
@@ -4,6 +4,7 @@ import cv2
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
 from pylab import *
+import sys
 
 # Dir for images
 image_location = '/Users/matt/github/Keras-CompVis/data/one_cell/'
@@ -17,41 +18,56 @@ glob_dir = image_location + jpg
 image_list = glob.glob(image_location + jpg)
 # print image_list
 
+# Filename for cell locations
+outfile = 'Cell_locations.csv'
 
 # output location
-image_location_output = '/Users/matt/github/Keras-CompVis/data/one_cell_patches'
+location_output = '/Users/matt/github/Keras-CompVis/data/one_cell_patches/'
+
+out_fh = open(location_output + outfile, 'r')
+lineList = out_fh.readlines()
+out_fh.close()
+
+done_files = []
+for line in lineList:
+    done_files.append(line.split(',')[0])
 
 
 class Pixel_locator():
 
+    # init first variables
     def __init__(self):
         self.fname = 'image.jpg'
-        self.point = ()
         self.point = ()
         self.point_list = []
         self.click_left = 3
 
+    # point collection fucntion
     def __onclick__(self, click):
-        self.point = (click.xdata, click.ydata)
+        self.point = (int(click.xdata), int(click.ydata))
         self.click_left -= 1
         return self.point
 
-    def __on_Declick__(self, declick):
-        self.fig.canvas.mpl_disconnect(self.cid_press)
-
+    # point collections and image display function
     def getCoord(self):
         self.img = Image.open(self.fname)
         width, height = self.img.size
         fig = plt.figure()
         ax = fig.add_subplot(111)
         imshow(self.img)
-        print self.click_left
+        print "Number of Clicks Left:", self.click_left
+
+        # Trigger click detection
         self.cid_press = fig.canvas.mpl_connect(
             'button_press_event', self.__onclick__)
+
+        # Annotate image with number of cells left to click
         ax.annotate(str(self.click_left), xy=(
             width * .9, height / 3), size=100, weight='bold', alpha=0.5)
         ax.annotate('Cells Left:', xy=(
             width * .8, height / 5), size=10, weight='bold', alpha=0.5)
+
+        # Displace circle on clicked cells for a give image
         for shape in self.point_list:
             el = Ellipse(
                 (shape[0], shape[1]), 20, 20, facecolor='r', alpha=0.5)
@@ -62,25 +78,31 @@ class Pixel_locator():
         plt.waitforbuttonpress()
         return self.point
 
-list = []
 zero = 1
 
 cell_locations = []
-for image in image_list[1:3]:
+for image in image_list[1:10]:
+    if image not in done_files:
+        image_name = image.split('/')[-1][0:-4]
+        cell_count_GT = int(image_name.split('_')[2].split('C')[1])
 
-    image_name = image.split('/')[-1][0:-4]
-    cell_count_GT = int(image_name.split('_')[2].split('C')[1])
+        print "Working on:", image_name, "--- %s" % (zero / len(image_list) * 100), '% done'
 
-    print "Working on:", image_name, "--- %s" % (zero / len(image_list) * 100), '% done'
+        click = Pixel_locator()
+        click.fname = image
+        click.click_left = cell_count_GT
 
-    click = Pixel_locator()
-    click.fname = image
-    click.click_left = 4
+        while click.click_left > 0:
+            count = click.getCoord()
+            click.point_list.append(count)
+            print count
+        else:
+            cell_locations.append([image, click.point_list])
 
-    while click.click_left > 0:
-        count = click.getCoord()
-        click.point_list.append(count)
-        print count
+            # open outfile in append mode
+            out_fh = open(location_output + outfile, 'a')
+            out_fh.write("%s, %s\n" % (image, click.point_list))
+            out_fh.close()
     else:
-        cell_locations.append([image, click.point_list])
-print cell_locations
+        continue
+# print cell_locations
