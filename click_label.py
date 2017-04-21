dd8fa3853effe7d9f755072e189d318f0dee0d7d
mattgrobelny <mattgrobelny@gmail.com>
2017-04-21 14:08:54 -0500

    working clicks

--- click_label.py
+++ click_label.py
@@ -23,81 +23,62 @@ image_list = glob.glob(image_location + jpg)
 image_location_output = '/Users/matt/github/Keras-CompVis/data/one_cell_patches'
 
 
-# class TestClass():
-#
-#     def __init__(self):
-#         self.fname = 'image.jpg'
-#         self.img = Image.open(self.fname)
-#         self.point = ()
-#
-#     def __onclick__(self, click):
-#         self.point = (click.xdata, click.ydata)
-#         return self.point
-#
-#     def getCoord(self):
-#         fig = plt.figure()
-#         ax = fig.add_subplot(111)
-#         imshow(self.img)
-#         #cid = fig.canvas.mpl_connect('button_press_event', self.__onclick__)
-#         plt.show()
-#         return self.point
-#
-
-
 class TestClass():
 
     def __init__(self):
-        self.fname = "image"
+        self.fname = 'image.jpg'
+        self.point = ()
         self.point = ()
         self.point_list = []
-        self.click_left = 1
+        self.click_left = 3
 
     def __onclick__(self, click):
         self.point = (click.xdata, click.ydata)
         self.click_left -= 1
         return self.point
 
+    def __on_Declick__(self, declick):
+        self.fig.canvas.mpl_disconnect(self.cid_press)
+
     def getCoord(self):
+        self.img = Image.open(self.fname)
         fig = plt.figure()
         ax = fig.add_subplot(111)
         imshow(self.img)
-        cid = fig.canvas.mpl_connect('button_press_event', self.__onclick__)
-        plt.show()
+        print self.click_left
+        self.cid_press = fig.canvas.mpl_connect(
+            'button_press_event', self.__onclick__)
+        plt.show(block=False)
+        plt.waitforbuttonpress()
         return self.point
-
-    # def getCoord(self):
-    #     self.img = Image.open(self.fname)
-    #     fig = plt.figure()
-    #     ax = fig.add_subplot(111)
-    #     imshow(self.img)
-    #     cid = fig.canvas.mpl_connect(
-    #         'button_press_event', self.__onclick__)
-    #
-    #     fig.canvas.mpl_disconnect(cid)
-    #     return self.point
 
 list = []
 zero = 1
 
+cell_locations = []
 for image in image_list[1:2]:
+
     image_name = image.split('/')[-1][0:-4]
     cell_count_GT = int(image_name.split('_')[2].split('C')[1])
-    cell_count_GT = 3
+    # cell_count_GT = 3
 
     print "Working on:", image_name, "--- %s" % (zero / len(image_list) * 100), '% done'
 
     click = TestClass()
+
     click.fname = image
     click.click_left = cell_count_GT
-    while click.click_left > len(click.point_list):
-        pts = click.getCoord()
-        print pts
-        click.point_list.append(pts)
-        print click.point_list
-    else:
-        continue
-    # print click.getCoord()
 
-    # try:
-    #     x, y = zip(*self.point_list)
-    #     plt.plot(x, y, 'g^')
+    # count = click.getCoord()
+    # click.point_list.append(count)
+    # print count
+    #
+    # cell_locations = click.point_list
+
+    while click.click_left > 0:
+        count = click.getCoord()
+        click.point_list.append(count)
+        print count
+    else:
+        cell_locations = click.point_list
+print cell_locations
