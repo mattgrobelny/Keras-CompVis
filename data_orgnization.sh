57ef8c4655886d19e093ec79e7b12c7c78cc5ad9
mattgrobelny <mattgrobelny@gmail.com>
2017-05-02 13:27:07 -0500

    added data org commands

--- data_orgnization.sh
+++ data_orgnization.sh
@@ -1,6 +1,12 @@
 # Prepping Training data
-
+# Copy patches
 cp `ls -1 | grep -E "_CX_0"`  ../Working_Sets_Patches/Training/0/
 cp `ls -1 | grep -E "_CX_1"`  ../Working_Sets_Patches/Training/1/
 cp `ls -1 | head -n 4000 | grep -E "_CX_1"`  ../Working_Sets_Patches/Training/1/
 cp `ls -1 | tail -n 5000 | grep -E "_CX_1"`  ../Working_Sets_Patches/Training/1/
+
+# Move random 20% from Training 0 or 1 into test and Validation 
+mv `ls -1 | shuf | head -n 1339` ../../Test/1/
+mv `ls -1 | shuf | head -n 1339` ../../Validation/1/
+mv `ls -1 | shuf | head -n 487` ../../Validation/0/
+mv `ls -1 | shuf | head -n 487` ../../Test/0/
