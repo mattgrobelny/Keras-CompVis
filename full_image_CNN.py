f5fc9c50a5b5a709c093523bdc4c5f9e7a4e4674
mattgrobelny <mattgrobelny@gmail.com>
2017-05-12 04:32:49 -0500

    updated changes to Fcnn

--- full_image_CNN.py
+++ full_image_CNN.py
@@ -163,15 +163,14 @@ model.add(MaxPooling2D(pool_size=(3, 3)))
 # 2D Conv 3 - None trainable
 model.add(Conv2D(64, (3, 3), trainable=False))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(3, 3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
 # 2D Conv 4 - None trainable
 model.add(Conv2D(128, (3, 3), trainable=False))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(3, 3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
-
 # Image detecting  Layers - End
 
 # Layers for patch training recognition
