7442c0123dd366ae3e3153fda074e937182b6837
mattgrobelny <mattgrobelny@gmail.com>
2017-05-12 03:32:38 -0500

    fix too may ages

--- full_image_CNN.py
+++ full_image_CNN.py
@@ -375,8 +375,8 @@ report_fh.write("Image Dir,Image_name,GroundTruth, P_None_Nuc, P_Nuclei \n")
 for i in range(len(model_predict)):
     image_name = image_list[i].split('/')[-1][0:-4]
     image_cat = int(image_name.split('_')[2].split('C')[1])
-    report_fh.write("%s,%s,%s,%s,%s \n" % (image_list[i], image_name, image_cat,
-                                           model_predict[0][i]))
+    report_fh.write("%s,%s,%s,%s \n" % (image_list[i], image_name, image_cat,
+                                        model_predict[0][i]))
 print("Done!")
 report_fh.close()
 
@@ -395,7 +395,7 @@ report_fh.write(
 for i in range(len(model_predict)):
     image_name = image_list[i].split('/')[-1][0:-4]
     image_cat = int(image_name.split('_')[2].split('C')[1])
-    report_fh.write("|![image](%s)|%s|%s|%s|%s| \n" % (image_list[i].split('/')[-1], image_name, image_cat,
-                                                       model_predict[0][i]))
+    report_fh.write("|![image](%s)|%s|%s|%s| \n" % (image_list[i].split('/')[-1], image_name, image_cat,
+                                                    model_predict[0][i]))
 print("Done!")
 report_fh.close()
